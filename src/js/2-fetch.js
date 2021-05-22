7c86e2726315fd7a9694fe908429268a4b84bba1
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-05-22 19:57:38 +0200

    Add js fetch and search

--- src/js/2-fetch.js
+++ src/js/2-fetch.js
@@ -1,34 +1,38 @@
-let globalShows = [];
-let favoriteShows = [];
-
-//Llamamos a la API
-
 function getDataShows(){
     let inputValue = inputElement.value; //Necesitamos el valor del input para incluirlo en la url del fetch
-    const imgDefault = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
 
     fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`)
         .then(response => response.json())
         .then((data) => {
-            console.log(data); //Nos devuelve un array de objetos
-            renderList(data);
+            //console.log(data); //Nos devuelve un array de objetos con series
+            globalShows = data; //Asignamos el array de objetos a una variable
+            //console.log(globalShows);
+            renderList(globalShows);
         });
 }
 
 function renderList(data){
-    
-            /*for (let i=0; i<data.length; i++){
-                const nameShows = data[i].show.name;
-                let imgShow = data[i].show.image;
+    ulElement.innerHTML = "";
+    //const show = data[0].show;
+    //console.log(show);
 
-                if(imgShow === null){
-                    imgDefault;
-                } else {
-                    imgShow.medium;
-                }
-            }*/
-            
-            //paintShowsInResults();
+    for (let i=0; i<data.length; i++){
+        let show = data[i].show;
+        let imgShow = show.image.medium;
+        let titleShow = show.name;
+
+        ulElement.innerHTML += `
+        <li id="${show.id}" class="js-resultList-item list-item">
+            <img src="${imgShow}" alt="Imagen ${titleShow} class="js-img"> 
+            <p>${titleShow}</p>
+        </li>`;
+
+        if(imgShow === null){
+            imgDefault;
+        } else {
+            imgShow;
+        }
+    }
 }
 
 function handleSearch(){
