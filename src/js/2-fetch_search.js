eef0e7ad0483ff357c189b28bfed39db9b0a2e5e
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-05-23 01:09:49 +0200

    Add part of favorites

--- src/js/2-fetch_search.js
+++ src/js/2-fetch_search.js
@@ -1,7 +1,7 @@
 /*
 1- Llamar a la API
 2- Por cada show contenido en el resultado de la búsqueda hay que pintar una tarjeta donde mostramos imagen y título de la serie
----3--- Series sin imagen --> mostrar imagen de relleno: https://via.placeholder.com/210x295/ffffff/666666/?text=TV
+3- Series sin imagen --> mostrar imagen de relleno: https://via.placeholder.com/210x295/ffffff/666666/?text=TV
  */
 
 
@@ -12,7 +12,7 @@ function getDataShows(){
         .then(response => response.json())
         .then((data) => {
             console.log(data); //Nos devuelve un array de objetos con series
-            globalShows = data; //Asignamos el array de objetos a una variable
+            globalShows = data; //Asignamos el array de objetos a una variable para poder pasar los datos a otra función y poder usarlos en más ocasiones
             //console.log(globalShows);
             renderList(globalShows);
         });
@@ -20,36 +20,39 @@ function getDataShows(){
 
 
 function renderList(data){
-    ulElement.innerHTML = "";
+    //ulElement.innerHTML = ""; --> Nos borraría lo que hubiese en la lista HTML antes de pintar las paletas
     //const show = data[0].show;
     //console.log(show);
 
     for (let i=0; i<data.length; i++){
         let show = data[i].show;
-        let imgShow = data[i].show.image.medium;
-        //console.log(imgShow);
         let titleShow = data[i].show.name;
 
 
         if(data[i].show.image != null){
+            let imgShow = data[i].show.image.medium;
+
             ulElement.innerHTML += 
-            `<li id="${show.id}" class="js-resultList-item list-item">
+            `<li id="${show.id}" class="js-resultList-item list-item favorite">
                 <img src="${imgShow}" alt="Imagen ${titleShow} class="js-img"> 
                 <p>${titleShow}</p>
             </li>`;
-            
         } else {
-            ulElement.innerHTML += `
-            <li id="${show.id}" class="js-resultList-item list-item">
+            ulElement.innerHTML += 
+            `<li id="${show.id}" class="js-resultList-item list-item favorite">
                 <img src="${imgDefault}" alt="Imagen ${titleShow} class="js-img">
+                <p>${titleShow}</p>
             </li>`;
         }
     }
+
+    addListenerLi(); //Cuando terminemos de pintar el resultado con la lista de series, se añaden los listener a cada li
 }
 
 
 function handleSearch(){
     getDataShows();
+    addListenerLi();
 }
 
 searchElement.addEventListener('click', handleSearch);
--- src/js/3-favorites.js
+++ src/js/3-favorites.js
@@ -1,6 +1,30 @@
 /* Click serie favorita:
-Color de fondo y fuente se intercambian
-Mostrar listado en parte izq con series seleccionadas
-Series favoritas siguen apareciendo aunque usuaria realice otra búsqueda
+1- Color de fondo y fuente se intercambian --> toggle al li
+2- Mostrar listado en parte izq con series seleccionadas
+3- Series favoritas siguen apareciendo aunque usuaria realice otra búsqueda
 */
+
+function addListenerLi(){
+    //Le añadimos un listener a todos los <li> 
+    const allShows = document.querySelectorAll('.js-resultList-item'); //Nos devolverá un array
+
+    //A cada <li> seleccionado, le añadimos/quitamos como favorito
+    for(const show of allShows){
+        show.classList.remove('favorite');
+        show.addEventListener('click', handleClickFav);
+    }
+}
+
+function handleClickFav(event){
+    //console.log(event.target);
+    //Con currentTarget seleccionamos todo el elemento <li>. Sin embargo, con target únicamente seleccionamos cada elemento de <li> por separado
+
+    const favShowClicked = event.currentTarget;
+
+    favShowClicked.classList.toggle('favorite');
+    
+}
+
+
+
 
