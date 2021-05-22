002ad184c6ff808e7cf89d267559b70577090d5d
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-05-22 18:34:57 +0200

    Add changes HTML

--- src/js/2-fetch.js
+++ src/js/2-fetch.js
@@ -4,13 +4,20 @@ let favoriteShows = [];
 //Llamamos a la API
 
 function getDataShows(){
-    let inputValue = inputElement.value; //Necesitamos el valor del input para incluirlo en url del fetch
+    let inputValue = inputElement.value; //Necesitamos el valor del input para incluirlo en la url del fetch
     const imgDefault = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
 
     fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`)
         .then(response => response.json())
         .then((data) => {
-            for (let i=0; i<data.length; i++){
+            console.log(data); //Nos devuelve un array de objetos
+            renderList(data);
+        });
+}
+
+function renderList(data){
+    
+            /*for (let i=0; i<data.length; i++){
                 const nameShows = data[i].show.name;
                 let imgShow = data[i].show.image;
 
@@ -19,16 +26,14 @@ function getDataShows(){
                 } else {
                     imgShow.medium;
                 }
-            }
+            }*/
             
             //paintShowsInResults();
-        });
 }
-
 
 function handleSearch(){
     getDataShows();
-};
+}
 
 
-buttonElement.addEventListener('click', handleSearch);
+searchElement.addEventListener('click', handleSearch);
